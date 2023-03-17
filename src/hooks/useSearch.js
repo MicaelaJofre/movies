99331b8a2b699749c717b2bf0df81633a751fc55
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-17 00:49:21 -0300

    validations eliminated

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from "react"
 
 export const useSearch=()=>{
   const [search, setSearch]= useState('')
-  const [error, setError] = useState('')
   const isFirstInput = useRef(true)
 
   
@@ -11,16 +10,11 @@ export const useSearch=()=>{
       isFirstInput.current = search === ''
       return
     }
-    if(search === '') return setError('You must enter a movie to search for')
-    if(search.length < 3) return setError('Must have at least three characters')
-    if(search.match(/^\d+$/)) return setError(`You can't search for a movie with a number`)
-    setError('')
   }, [search])
   
 
     return{
         search,
         setSearch,
-        error
     }
 }
