a3bfd13a4504798c116fe29e9dd359691f825ca6
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-21 22:23:09 -0300

    Modification of image sizes

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,13 +2,13 @@
 
 const ListMovies=({movies})=>{
     return(
-        <div className='grid grid-cols-movies px-32 py-20 gap-7 gap-y-20'>
+        <div className='grid grid-cols-movies px-20 py-20 gap-10 gap-y-20'>
         {
             movies.map(movie=>{
             return(
                 <div className="flex flex-col gap-2 justify-between relative" key={movie.id}>
-                    <h3 className="pl-2 opacity-60 text-[12px] font-semibold w-60 whitespace-nowrap overflow-ellipsis overflow-hidden">{movie.title}</h3>
-                    <img className="w-full h-[370px] object-cover rounded-lg" src={movie.poster} alt= {movie.description}/>
+                    <h3 className="pl-2 opacity-60 text-[12px] font-semibold w-40 whitespace-nowrap overflow-ellipsis overflow-hidden">{movie.title}</h3>
+                    <img className="w-[200px] h-full object-cover rounded-lg" src={movie.poster} alt= {movie.description}/>
                 </div>
             )
             })
