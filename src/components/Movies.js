cca2a93e19d5170e38bfebdf602923d87437efa2
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-22 23:24:09 -0300

    Added style to movie cards

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,40 +1,42 @@
 
 
 const ListMovies=({movies})=>{
-    return(
-        <div className='grid grid-cols-movies px-20 py-20 gap-10 gap-y-20'>
-        {
-            movies.map(movie=>{
-            return(
-                <div className="flex flex-col gap-2 justify-between relative" key={movie.id}>
-                    <h3 className="pl-2 opacity-60 text-[12px] font-semibold w-40 whitespace-nowrap overflow-ellipsis overflow-hidden">{movie.title}</h3>
-                    <img className="w-[200px] h-full object-cover rounded-lg" src={movie.poster} alt= {movie.description}/>
-                </div>
-            )
-            })
-        }
-        </div>
-    )
+  return(
+    <div className='grid grid-cols-movies px-10 py-20 gap-1 gap-y-20'>
+    {
+        movies.map(movie=>{
+        return(
+            <div className="flex flex-col w-[180px] justify-between relative rounded-lg border-[3px] border-opacity-10 border-gray-700" key={movie.id}>
+                <img className="h-full object-cover " src={movie.poster} alt= {movie.description}/>
+                <span className="absolute w-full h-11 backdrop-blur-xl backdrop-opacity-75 bottom-0"></span>
+                <h3 className="p-4 pl-2 uppercase text-white text-[10px] font-semibold whitespace-nowrap overflow-ellipsis overflow-hidden">{movie.title}</h3>    
+            </div>
+        )
+        })
+    }
+    </div>
+) 
 }
 
 const NotSearchMovies =()=>{
-    return(
-        <h3>No results for your search</h3>
-    )
+  return(
+    <div className="w-full flex flex-col justify-center items-center text-red">
+        <h3 className="text-ms">No results for your search</h3>
+        <p className="text-xs opacity-60">Try trying a different movie title</p>
+    </div>
+)
 }
 
 export const Movies =({movies})=>{
-    
-    
-    return(
-        <>
-        {
-            movies?.length > 0
-            ? <div className='movies'>
-                <ListMovies movies={movies}/>
-            </div>
-            : <NotSearchMovies/>
-        }
-        </>
-    )
+  return(
+    <>
+    {
+        movies?.length > 0
+        ? <div className='movies'>
+            <ListMovies movies={movies}/>
+        </div>
+        : <NotSearchMovies/>
+    }
+    </>
+)
 }
