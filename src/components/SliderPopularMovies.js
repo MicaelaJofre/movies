7fa96bde96305d2fe83f60737e69b28f77be5a7f
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-24 02:04:50 -0300

    Slider implementation with tailwind

--- src/components/SliderPopularMovies.js
+++ src/components/SliderPopularMovies.js
@@ -1,50 +1,23 @@
 import { useSliderMovies } from "../hooks/useSliderMovies"
-import { Swiper, SwiperSlide } from "swiper/react"
-import SwiperCore, { Autoplay } from 'swiper'
 
-// Import Swiper styles
-import "swiper/css"
-import 'swiper/swiper-bundle.css'
-
-SwiperCore.use([Autoplay])
 
 const SliderPopularMovies =()=>{
   const {listPopularMovies} = useSliderMovies()
 
-    return(
-        <div className='absolute w-full l-0 opacity-30 pt-24'> 
-          <Swiper 
-          navigation={false} 
-          slidesPerView={8}
-          spaceBetween={0}
-          //preloadImages={true}
-          //centeredSlides={true}
-          autoplay={{
-            delay: 300,
-            disableOnInteraction: false
-          }}
-          speed={1200}
-          effect={'slide'}
-          loop={true}
-          //loopAdditionalSlides={8}
-          grabCursor={true}
-          className="overflow-visible">
-            {
-              listPopularMovies?
-              listPopularMovies.map(list =>{
-                return (
-                  <SwiperSlide key={list.id} className='flex justify-center'>
-                    <div className="px-2">
-                      <img className='object-contain ' alt={`imagen del cartel ${list.poster}`} src={list.poster}/>
-                    </div>
-                  </SwiperSlide>
+  return(
+    <div className="h-[250px] m-auto overflow-hidden relative w-auto" >
+      <div className="flex w-[calc(180px*20)] gap-4 opacity-40 animate-scroll items-center">
+        {
+          listPopularMovies &&
+          listPopularMovies.map(list =>{
+            return (
+                <img key={list.id} className='object-cover h-full w-[180px]' alt={`url poster: ${list.poster}`} src={list.poster}/>
                 )
               })
-              
-            :null}
-          </Swiper>
-        </div>
-    )
+            } 
+      </div>
+    </div>
+  )
 }
 
 export default SliderPopularMovies
