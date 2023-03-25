b9582e4bdf5ba5975ebc1ed302ab43eb4a4622af
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-25 02:24:32 -0300

    Media querys movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,11 @@
 const ListMovies=({movies})=>{
   return(
-    <div className='grid grid-cols-movies px-10 py-20 gap-1 gap-y-20'>
+    <div className='grid items-center justify-center w-[80%] mx-auto my-28 gap-1 gap-y-20 sm:grid-cols-movies'>
     {
       movies.map(movie=>{
         return(
             <div className="flex flex-col w-[180px] justify-between relative rounded-lg border-[3px] border-opacity-10 border-gray-700" key={movie.id}>
-              <img className="h-full object-cover " src={movie.poster} alt= {movie.description}/>
+              <img className="h-full object-cover" src={movie.poster} alt= {movie.description}/>
               <span className="absolute w-full h-11 backdrop-blur-xl backdrop-opacity-75 bottom-0"></span>
               <h3 className="p-4 pl-2 uppercase text-white text-[10px] font-semibold whitespace-nowrap overflow-ellipsis overflow-hidden">{movie.title}</h3>    
             </div>
