954156d5589e638c6f0acd8a1d5026ab9f541928
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-24 23:39:00 -0300

    Created div and w-full

--- src/components/SliderPopularMovies.js
+++ src/components/SliderPopularMovies.js
@@ -5,18 +5,20 @@ const SliderPopularMovies =()=>{
   const {listPopularMovies} = useSliderMovies()
 
   return(
-    <div className="h-[250px] m-auto overflow-hidden relative w-auto" >
+    <section className="h-[250px] m-auto overflow-hidden relative w-full">
       <div className="flex w-[calc(180px*20)] gap-4 opacity-40 animate-scroll items-center">
         {
           listPopularMovies &&
           listPopularMovies.map(list =>{
             return (
-                <img key={list.id} className='object-cover h-full w-[180px]' alt={`url poster: ${list.poster}`} src={list.poster}/>
+                <div>
+                  <img key={list.id} className='object-cover h-full w-[180px]' alt={`url poster: ${list.poster}`} src={list.poster}/>
+                </div>
                 )
               })
             } 
       </div>
-    </div>
+    </section>
   )
 }
 
