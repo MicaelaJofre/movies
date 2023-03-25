09ed0f015216605bea4cd82d66c11b9b68292034
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-24 23:35:50 -0300

    Added icon and label checkbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import debounce from 'just-debounce-it'
 import { useCallback, useEffect, useState } from 'react'
+import IconMovie from './components/IconMovie'
 import { Movies } from './components/Movies'
 import SliderPopularMovies from './components/SliderPopularMovies'
 import { useMovies } from './hooks/useMovies'
@@ -47,11 +48,17 @@ const App = () => {
     <div className='w-full h-full min-h-[100vh] bg-black text-white font-Poppins'>
       <header className='w-full h-auto relative'>
         <div className='pt-5 pb-10 flex flex-col items-center gap-2 md:flex-row md:justify-around'>
+          <div className='flex gap-2'>
+          <IconMovie/>
           <h1 className="font-bold text-[28px]">Movies</h1>
+          </div>
           <form className='flex gap-4 h-10 z-20 items-center' onSubmit={handleSubmit}>
             <input className='rounded-lg px-3 py-1 bg-[#222] placeholder-opacity-20 placeholder-white outline-none' name='query' placeholder='Matrix, Train...' value={search} onChange={handleChange}/>
-            <input name='sort' type='checkbox' checked={sort} onChange={handleSort}/>
-            <button className='border-[2px] border-red px-2 py-1 rounded-lg text-red uppercase text-sm font-medium' type='submit'>Search</button>
+            <div className='select-none relative w-12 h-6 m-5'>
+              <input name='sort' type='checkbox' id="check-apple" checked={sort} onChange={handleSort}/>
+              <label for="check-apple" className='absolute top-0 left-0 w-12 h-6 rounded-[50%] cursor-pointer transition-all'></label>
+            </div>            
+            <button className='border-[2px] border-red px-2 py-1 rounded-lg text-stone-300 uppercase text-sm font-medium' type='submit'>Search</button>
           </form>
         </div>  
         <SliderPopularMovies/>
